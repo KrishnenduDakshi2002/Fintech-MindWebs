74c144c37e29bfb0833930da8d6ef8b479a27f35
krishnendu Dakshi <kdakshi2020@gamil.com>
2023-01-21 00:43:21 +0530

    Change HOST from localhost

--- Fintech-client/src/Host.ts
+++ Fintech-client/src/Host.ts
@@ -1,2 +1,3 @@
 // export const HOST = "http://127.0.0.1:5001/aagman-api-firebase-a3d16/asia-south1/aagman/api"
-export const HOST = "http://localhost:3000/api"
+// export const HOST = "http://localhost:3000/api"
+export const HOST = "https://asia-south1-aagman-api-firebase-a3d16.cloudfunctions.net/aagman/api"
